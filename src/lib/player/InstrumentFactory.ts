4830fecb9b024053567795f2ac5d04e8b1317aa0
Adam Yarris <adamyarris@gmail.com>
2025-02-23 06:43:46 -0500

    Added some stuff

--- src/lib/player/InstrumentFactory.ts
+++ src/lib/player/InstrumentFactory.ts
@@ -17,6 +17,8 @@ export function createSynthFromInstrument(instrument: BeatInstrument): any {
             return new Tone.Synth(instrument.properties);
         case "NoiseSynth":
             return new Tone.NoiseSynth(instrument.properties);
+        case "FMSynth":
+            return new Tone.FMSynth(instrument.properties);
 
         default:
             throw new Error(`Instrument type ${instrument.name} is not supported`);
